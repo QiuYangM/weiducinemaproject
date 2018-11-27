371be69a7a8c64adc0f8e393f9d64c0bcabe50e6
weitianxiang123 <1815835822@qq.com>
2018-11-27 11:17:10 +0800

    注释

--- app/src/main/java/com/bw/movie/MainActivity.java
+++ app/src/main/java/com/bw/movie/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //维度科技项目组测试一
+
     }
 }
